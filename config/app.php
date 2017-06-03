14891e6e6bf0e2d06b4d23426379895502095ddf
Barış Esen <barisesennn@gmail.com>
2017-06-03 13:15:15 +0300

    init

--- config/app.php
+++ config/app.php
@@ -120,7 +120,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => 'errorlog'
+    //'log' => env('APP_LOG', 'single'),
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
